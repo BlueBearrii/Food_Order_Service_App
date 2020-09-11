5a83dbb99afa34d92ddbd389f19bef0146715086
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-11 21:49:48 +0700

    :building_construction: Re-construction

--- FoodOrderService/App.js
+++ FoodOrderService/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Main from './src/components/main/Main';
+import Menu from './src/components/main/Menu';
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Main />
+        <Menu />
       </View>
     );
   }
