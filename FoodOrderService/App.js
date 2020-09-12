dae99692cac8479ed30e277215faa01220d9eab7
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-12 23:46:01 +0700

    :bug: Fixed bug

--- FoodOrderService/App.js
+++ FoodOrderService/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import Main from './src/components/main/Main';
-import Menu from './src/components/main/Menu';
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Menu />
+        <Main />
       </View>
     );
   }
