42862e2eabc4e979320b2cb2296f75ce97b31eff
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-13 02:09:14 +0700

    :pencil: Manual data

---
+++ FoodOrderService/src/components/main/orderLists.js
@@ -0,0 +1,10 @@
+const lists = [
+  {name: 'กะเพราเป็ด', price: 35},
+  {name: 'กะเพรากุ้ง', price: 35},
+  {name: 'กะเพราหมึก', price: 35},
+  {name: 'กะเพราทะเล', price: 35},
+  {name: 'กะเพราไก่', price: 35},
+  {name: 'กะเพราหมู', price: 35},
+];
+
+export default lists;
