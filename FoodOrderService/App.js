c3bfde64806ef364298dc35ea8d345d0b9815233
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-13 23:07:10 +0700

    :seedling: Update routes

--- FoodOrderService/App.js
+++ FoodOrderService/App.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import Main from './src/components/main/Main';
+import Basket from './src/components/basket/Basket';
+
+const Stack = createStackNavigator();
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Main />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Menu">
+          <Stack.Screen name="Menu" component={Main} />
+          <Stack.Screen name="Basket" component={Basket} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
