2a3f5f9ebf96805f0ee97d7b73ed6d1789f2578b
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-15 02:04:59 +0700

    :gem: New UI construction

--- FoodOrderService/App.js
+++ FoodOrderService/App.js
@@ -1,21 +1,53 @@
 import React, {Component} from 'react';
 import {Text, View, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Main from './src/components/main/Main';
 import Basket from './src/components/basket/Basket';
+import OrderLists from './src/components/orderLists/OrderLists';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {
+  faAddressBook,
+  faShoppingBasket,
+} from '@fortawesome/free-solid-svg-icons';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Menu">
-          <Stack.Screen name="Menu" component={Main} />
-          <Stack.Screen name="Basket" component={Basket} />
-        </Stack.Navigator>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              if (route.name === 'OrderLists') {
+                iconName = faAddressBook;
+              } else if (route.name === 'Basket') {
+                iconName = faShoppingBasket;
+              }
+              return (
+                <FontAwesomeIcon icon={iconName} size={size} color={color} />
+              );
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen
+            name="OrderLists"
+            component={OrderLists}
+            options={{tabBarLabel: 'รายการอาหาร'}}
+          />
+          <Tab.Screen
+            name="Basket"
+            component={Basket}
+            options={{tabBarLabel: 'ตะกร้า'}}
+          />
+        </Tab.Navigator>
       </NavigationContainer>
     );
   }
