8e76478f8d5274ba58b5c57164771e10409b5976
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-21 15:29:15 +0700

    :gem: Improve state management structure

---
+++ FoodOrderService/src/components/order_screen/OrderScreen.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {Button, Text, View, StyleSheet} from 'react-native';
+import OrderInBasket from './OrderInBasket';
+import OrderLists from './OrderLists';
+
+export default class OrderScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orderItems: ['Burger', 'Pizza'],
+      orderInBasket: [],
+    };
+  }
+  render() {
+    const onSelectOrder = (item) => {
+      this.setState({
+        orderInBasket: [...this.state.orderInBasket, item],
+      });
+      console.log(this.state);
+    };
+    return (
+      <View style={styles.container}>
+        <OrderLists
+          orderItems={this.state.orderItems}
+          onSelectOrder={onSelectOrder}
+        />
+        <OrderInBasket orderInBasket={this.state.orderInBasket} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+  },
+});
