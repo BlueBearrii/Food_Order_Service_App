e808d4eacf8840fcd331b2eb45da24694fe45783
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-21 15:27:59 +0700

    :zap: New structure

--- FoodOrderService/App.js
+++ FoodOrderService/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import OrderScreen from './src/components/order_screen/OrderScreen';
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Text> textInComponent </Text>
+        <OrderScreen />
       </View>
     );
   }
