523364147f0477a40cff1d400c9ab218c28e410c
BlueBearrii <thananan.math.acs@gmail.com>
2020-09-23 20:54:51 +0700

    :gem: Update function 'ADD' and 'REMOVE'

--- FoodOrderService/src/components/order_screen/OrderInBasket.js
+++ FoodOrderService/src/components/order_screen/OrderInBasket.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import OrderInBasketQuantity from './OrderInBasketQuantity';
 
 export default class OrderInBasket extends Component {
   render() {
-    const {orderInBasket} = this.props;
+    const {orderInBasket, removeItemFromBasket, onSetQuantity} = this.props;
     return (
       <View style={styles.container}>
         <Text style={styles.header}>ตะกร้า</Text>
-        <View style={styles.basket}>
-          {orderInBasket.map((item, index) => {
-            return <OrderInBasketQuantity key={index} item={item} />;
-          })}
-        </View>
+        <ScrollView>
+          <View style={styles.basket}>
+            {orderInBasket.map((item, index) => {
+              return (
+                <OrderInBasketQuantity
+                  key={index}
+                  item={item}
+                  index={index}
+                  removeItemFromBasket={removeItemFromBasket}
+                  onSetQuantity={onSetQuantity}
+                />
+              );
+            })}
+          </View>
+        </ScrollView>
       </View>
     );
   }
